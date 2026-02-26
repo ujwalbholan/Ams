c13e5816142447bf69531f0b12f5df0dd54c0eaa
ujwal bholan <160625871+ujwalbholan@users.noreply.github.com>
2026-02-26 20:18:34 +0545

    landing page,login,register, dashboard page added

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function DashboardHome() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Welcome to Admin Dashboard</h1>
+      <p className="text-gray-700">
+        Use the sidebar to navigate between Users, Artists, and other management
+        pages.
+      </p>
+    </div>
+  );
+}
