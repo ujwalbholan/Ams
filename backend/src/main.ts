9f8914184487b48282c45be3dd3a143241e4bf23
ujwal bholan <160625871+ujwalbholan@users.noreply.github.com>
2026-02-27 20:28:10 +0545

    delet,e changer to delete

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELET,E,OPTIONS',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
