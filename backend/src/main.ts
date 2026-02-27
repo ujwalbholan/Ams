d3af8679920701cdf9caf0bbeb2c9456f598e9e4
ujwal bholan <160625871+ujwalbholan@users.noreply.github.com>
2026-02-27 20:33:25 +0545

    origin added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://ams-ashen.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
