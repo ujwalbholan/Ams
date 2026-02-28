e5382f28fee47e5d64b0cd21ccc62bf9ce6103e0
ujwal bholan <160625871+ujwalbholan@users.noreply.github.com>
2026-02-28 12:22:05 +0545

    minor change 0.0.1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://ams-ashen.vercel.app'],
+    origin: ['http://localhost:3000', 'https://ams-ashen.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
